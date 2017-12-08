a6b577a79ac8f4c78cd83a1fc77c389a3f43db49
dwayne1516 <dariuswscott@gmail.com>
2017-12-08 04:14:16 -0800

    Update Song.java

--- src/main/java/com/example/demo/Song.java
+++ src/main/java/com/example/demo/Song.java
@@ -1,3 +1,4 @@
+package com.example.demo;
 public class Song {
     private long id;
     private String name;
@@ -12,5 +13,45 @@ public class Song {
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 }
