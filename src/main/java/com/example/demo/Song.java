6d8f9628891ee9f7ca2aba8ac69aab15edfe1d62
dwayne1516 <dariuswscott@gmail.com>
2017-12-08 05:19:59 -0800

    Initial commit

---
+++ src/main/java/com/example/demo/Song.java
@@ -0,0 +1,16 @@
+public class Song {
+    private long id;
+    private String name;
+    private String artist;
+    private String album;
+    private int rating;
+    private int year;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
